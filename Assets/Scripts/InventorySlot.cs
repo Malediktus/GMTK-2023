dd2321e8f6b1aff66f4710aea629d063e8f3beb1
Nico Grundei <malediktusrgb@gmail.com>
2023-07-08 00:58:52 +0200

    Drag and Drop system

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    void IDropHandler.OnDrop(PointerEventData eventData)
+    {
+        GameObject dropped = eventData.pointerDrag;
+        DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+        if (!draggableItem)
+            return;
+
+        draggableItem.SetParentAfterDrag(transform);
+    }
+}
