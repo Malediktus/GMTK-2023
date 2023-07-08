700b669e9b6d1d2091e0e84c0ffaed405ff39090
Nico Grundei <malediktusrgb@gmail.com>
2023-07-08 16:27:12 +0200

    Character instance and prefab

--- Assets/Scripts/NPCManager.cs
+++ Assets/Scripts/NPCManager.cs
@@ -7,30 +7,55 @@ public class NPCManager : MonoBehaviour
 {
     [SerializeField] private int numCharactersAtStart = 5;
     [SerializeField] private Vector2 characterSpawnRate = new Vector2(300, 600);
-    [SerializeField] private Vector2 characterVisitRate = new Vector2(60, 180);
     [SerializeField] private UnityEvent<NonPlayableCharacter> onVisitEvent = new UnityEvent<NonPlayableCharacter>();
     [SerializeField] private List<Sprite> characterSprites;
-    private List<NonPlayableCharacter> characters;
+    [SerializeField] private List<string> characterNames;
+    [SerializeField] private Transform canvas;
+    [SerializeField] private GameObject characterPrefab;
+    private List<NonPlayableCharacter> characters = new List<NonPlayableCharacter>();
+    private NonPlayableCharacter currentVisitor;
+    private GameObject currentVisitorInstance;
 
     private void Start()
     {
+        currentVisitor = null;
+        currentVisitorInstance = null;
         for (int i = 0; i < numCharactersAtStart; i++)
         {
-            characters.Add(new NonPlayableCharacter(characterSprites[Random.Range(0, characterSprites.Count - 1)]));
+            string name = characterNames[Random.Range(0, characterNames.Count - 1)];
+            characterNames.Remove(name); // Dont reuse names
+            characters.Add(new NonPlayableCharacter(characterSprites[Random.Range(0, characterSprites.Count - 1)], name));
         }
         Invoke("SpawnCharacter", Random.Range(characterSpawnRate.x, characterSpawnRate.y));
-        Invoke("CharacterVisit", Random.Range(characterVisitRate.x, characterVisitRate.y));
+        CharacterVisit(characters[Random.Range(0, characters.Count - 1)]);
     }
 
     private void SpawnCharacter()
     {
-        characters.Add(new NonPlayableCharacter(characterSprites[Random.Range(0, characterSprites.Count - 1)]));
+        string name = characterNames[Random.Range(0, characterNames.Count - 1)];
+        characterNames.Remove(name); // Dont reuse names
+        characters.Add(new NonPlayableCharacter(characterSprites[Random.Range(0, characterSprites.Count - 1)], name));
         Invoke("SpawnCharacter", Random.Range(characterSpawnRate.x, characterSpawnRate.y));
     }
 
-    private void CharacterVisit()
+    private void CharacterVisit(NonPlayableCharacter character)
     {
-        onVisitEvent?.Invoke(characters[Random.Range(0, characters.Count - 1)]);
-        Invoke("CharacterVisit", Random.Range(characterVisitRate.x, characterVisitRate.y));
+        currentVisitor = character;
+        GameObject prefabInstance = Instantiate(characterPrefab, canvas);
+        prefabInstance.transform.SetAsFirstSibling();
+        currentVisitorInstance = prefabInstance;
+        onVisitEvent?.Invoke(character);
+        Dialogue.Instance.Tell(new List<string> { "A new visitor arrived!", "You can buy stuff from him or sell him stuff." }); // TODO: Fix
+    }
+
+    public void OnEndDialog()
+    {
+        if (currentVisitor == null)
+        {
+            return;
+        }
+        Destroy(currentVisitorInstance);
+        currentVisitor = null;
+        currentVisitorInstance = null;
     }
 }
--- Assets/Scripts/NonPlayableCharacter.cs
+++ Assets/Scripts/NonPlayableCharacter.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class NonPlayableCharacter
 {
     public List<ShopItem> inventory;
+    public string name;
+    private Sprite sprite;
 
-    public NonPlayableCharacter(Sprite sprite)
+    public NonPlayableCharacter(Sprite _sprite, string _name)
     {
+        sprite = _sprite;
+        name = _name;
     }
 }
