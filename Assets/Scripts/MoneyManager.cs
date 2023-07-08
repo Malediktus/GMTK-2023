df93aac85a92621fa82e845887a5eafe0f681bce
MrLucky974 <HeroCreepyFR@gmail.com>
2023-07-08 13:34:36 +0400

    Tidying up MoneyManager.cs

--- Assets/Scripts/MoneyManager.cs
+++ Assets/Scripts/MoneyManager.cs
@@ -1,34 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Events;
 
 public class MoneyManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text moneyCount;
     [SerializeField] private float startMoney = 100.0f;
-    private float money;
+    private float Money {
+        get {
+            return Money;
+        }
+
+        // I don't know if this works, but it should invoke onMoneyChange when setting the value
+        set {
+            Money = value;
+            onMoneyChange?.Invoke();
+        }
+    }
+
+    UnityEvent onMoneyChange;
 
     private void Start()
     {
-        money = startMoney;
-        moneyCount.text = "Cash: " + startMoney;
+        Money = startMoney;
+        onMoneyChange.AddListener(UpdateText);
+    }
+
+    /// <summary>
+    /// Update the money count text. Prevent from repeating the same code over and over.
+    /// </summary>
+    private void UpdateText() {
+        moneyCount.text = $"Cash: {Money}";
     }
 
     public float GetMoney()
     {
-        return money;
+        return Money;
     }
 
     public void AddMoney(float amount)
     {
-        money += amount;
-        moneyCount.text = "Cash: " + money;
+        Money += amount;
+        //onMoneyChange?.Invoke();
     }
 
     public void SubMoney(float amount)
     {
-        money -= amount;
-        moneyCount.text = "Cash: " + money;
+        Money -= amount;
+        //onMoneyChange?.Invoke();
     }
 }
